c3b7a8dc173722b4b5327b59d09de1749b782fd3
gergokutu <gergo.kutu@gmail.com>
2020-05-28 12:09:10 +0200

    Next 10 days avg temp on screen

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -6,7 +6,7 @@
       <div class="select-box">
         <select v-model="countryCode">
           <option
-            v-for="(item, index) in cityList"
+            v-for="(item, index) in countryList"
             :key="index"
             :value="item.countryCode"
           >
@@ -25,20 +25,27 @@
         />
       </div>
 
-      <div
-        class="result"
-        v-if="
-          queryCity &&
-            queryCity.toLowerCase() === cityInfo.city_name &&
-            cityInfo.city_name.toLowerCase()
-        "
-      >
+      <div class="result" v-if="cityInfo.city_name !== undefined">
         <!-- <h3 class="city-name">{{ cityInfo.city_name }}</h3> -->
         <div class="actual-temp">
-          {{ Math.round(cityInfo.app_temp) }} &#8451;
+          {{ Math.round(cityInfo.data[0].temp) }} &#8451;
+        </div>
+        <div class="avg-10">
+          {{ Math.round(cityInfo.data[1].temp) }} &#8451;
         </div>
         <!-- <div class="date-time">{{ cityInfo.datetime }}</div> -->
-        <div class="forecast">next 7 days...</div>
+        <div class="forecast">
+          {{ cityInfo.data.map(day => day.temp).slice(1, 11) }}
+        </div>
+        <div class="forecast">
+          {{
+            cityInfo.data
+              .map(day => day.temp)
+              .slice(1, 11)
+              .reduce((acc, curr) => acc + curr) /
+              cityInfo.data.slice(1, 11).length
+          }}
+        </div>
       </div>
     </div>
   </div>
@@ -50,7 +57,7 @@ import { mapState } from "vuex";
 
 @Component({
   computed: {
-    ...mapState(["cityInfo", "cityList"])
+    ...mapState(["cityInfo", "countryList", "avgTen"])
   }
 })
 export default class Weather extends Vue {
